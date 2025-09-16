5a0d6e939bc03c8203fa0f606ad4d252aa701f0a
Trey Owen <treyeowen@gmail.com>
2025-09-15 21:10:22 -0500

    Delete old code and move files

--- GUI/needed.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(window)
-    window.show()
-    sys.exit(app.exec_())
