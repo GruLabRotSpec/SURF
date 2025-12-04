041e35ee1dc23ce6ec42b77c625b06a82898f79d
Trey Owen <treyeowen@gmail.com>
2025-12-04 16:24:49 -0600

    Add features to "test" file

--- src/spectrometer_gui/test.py
+++ src/spectrometer_gui/test.py
@@ -1,12 +1,40 @@
 from spectrometer import Spectrometer
 
+# This file is for basic testing and running of the spectrometer without the GUI
+# Actual testing is done in the test/ directory
+
+# Variables in arrows (<>) need to be replaced with actual values to run
+
 
 def main():
     spectrometer = Spectrometer()
 
     # Test frequency scan
-    spectrometer.scan_frequency(None, 11200, 0.5)
+    def frequencyScan():
+        spectrometer.scan_frequency(11000, 11200, 0.5)
 
+    # Test cavity search and frequency scan
+    def cavitySearchAndFrequencyScan():
+        spectrometer.set_instrument_settings(<zaber_speed>, <rf_level>)
+
+        # Cavity search
+        spectrometer.__oscilloscope_controller.recall_setup(<folder>, <file>)
+        spectrometer.set_output_options(<folder_name>, <filename>)
+        spectrometer.set_experiment_settings(<trig_rate>, <acq_rate>, <gate_pos>, <intensity>, <awg_freq>)
+        spectrometer.cavity_search(<stop_freq>, <step_size>)
+
+        # Frequency scan
+        spectrometer.__oscilloscope_controller.recall_setup(<folder>, <file>)
+        spectrometer.set_output_options(<folder_name>, <filename>)
+        spectrometer.set_experiment_settings(<trig_rate>, <acq_rate>, <gate_pos>, <intensity>, <awg_freq>)
+        spetrometer.scan_frequency(<start_freq>, <stop_freq>, <step_size>)
+
+
+    # Enter statements to run the above functions below this line
+
+    # cavitySearchAndFrequencyScan()
+
+    # Enter statemetns to run the above functions above this line
 
 if __name__ == "__main__":
     main()
