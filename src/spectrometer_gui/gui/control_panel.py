c3acb16604a7832a2c83369c264645dd10c0e113
Trey Owen <treyeowen@gmail.com>
2025-12-17 13:44:54 -0600

    Add settings and help window

--- src/spectrometer_gui/gui/control_panel.py
+++ src/spectrometer_gui/gui/control_panel.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget, QPushButton
 
+from gui.settings_window import SettingsWindow
 
 class ControlPanel(QWidget):
     def __init__(self):
@@ -11,3 +12,11 @@ class ControlPanel(QWidget):
         layout.addWidget(label)
 
         self.setLayout(layout)
+
+        show_button = QPushButton("View additional settings...")
+        show_button.clicked.connect(show_more_settings)
+        layout.addWidget(show_button)
+
+    def show_more_settings(self):
+        self.settings_window = SettingsWindow()
+        self.settings_window.show()
