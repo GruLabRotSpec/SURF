c53f3c0a18aca3d6f79066f5ce676642da9031fe
gamedungeon <gamedungeon@protonmail.com>
2026-02-11 14:15:32 -0600

    Update Nix

--- flake.nix
+++ flake.nix
@@ -5,60 +5,56 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      ...
-    }:
-    let
-      inherit (nixpkgs) lib;
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
 
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
 
-      python = pkgs.python313;
-    in
-    {
-      devShell.x86_64-linux = pkgs.mkShell {
-        packages = with pkgs; [
-          python
-          black
-          uv
+    python = pkgs.python313;
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      packages = with pkgs; [
+        python
+        black
+        uv
 
-          qt6.qtbase
-          qt6.qtdeclarative
-          qt6.qttools
-          qt6.qtsvg
-          qt6.qtwayland
+        qt6.qtbase
+        qt6.qtdeclarative
+        qt6.qttools
+        qt6.qtsvg
+        qt6.qtwayland
+      ];
 
-        ];
-
-        env = {
-          UV_PYTHON_DOWNLOADS = "never";
-          UV_PYTHON = python.interpreter;
-          LD_LIBRARY_PATH = lib.makeLibraryPath (
-            pkgs.pythonManylinuxPackages.manylinux1
-            ++ (with pkgs; [
-              dbus
-              mono
-              zstd
-              libGL
-              libxcb
-              libxkbcommon
-              fontconfig
-              freetype
-              xorg.libX11
-              xorg.libXcursor
-              xorg.libXrandr
-              wayland
-              wayland-protocols
-            ])
-          );
-        };
+      env = {
+        UV_PYTHON_DOWNLOADS = "never";
+        UV_PYTHON = python.interpreter;
+        LD_LIBRARY_PATH = lib.makeLibraryPath (
+          pkgs.pythonManylinuxPackages.manylinux1
+          ++ (with pkgs; [
+            dbus
+            mono
+            zstd
+            libGL
+            libxcb
+            libxkbcommon
+            fontconfig
+            freetype
+            libX11
+            libXcursor
+            libXrandr
+            wayland
+            wayland-protocols
+          ])
+        );
       };
     };
+  };
 }
