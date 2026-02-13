4f4bd427f7aebbf9453e2a5e7a0a8f1919a9af64
cademiller24 <162280569+cademiller24@users.noreply.github.com>
2026-02-13 16:41:52 -0600

    Fix Zaber class

--- src/spectrometer_gui/zaber_controller.py
+++ src/spectrometer_gui/zaber_controller.py
@@ -10,7 +10,7 @@ class ZaberController:
 
     def initialize(self, config: Config):
         port = Connection.open_serial_port(config.zaber_controller.zaber_port)
-
+        port.detect_devices() # Requires Internet connection on first run, then caches
         self.device = port.get_device(1)
         self.axis = self.device.get_axis(1)
 
@@ -29,7 +29,7 @@ class ZaberController:
         self.axis.move_absolute(
             pos,
             Units.LENGTH_MILLIMETRES,
-            velocity=self.max_speed,
+            velocity=self.move_speed,
             velocity_unit=Units.VELOCITY_MILLIMETRES_PER_SECOND,
         )
 
