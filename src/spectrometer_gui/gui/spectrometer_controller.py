7e199ab6c081ed3cde2ea9ab0bedfe8bfec217e1
gamedungeon <gamedungeon@protonmail.com>
2026-03-23 14:04:20 -0500

    Graph Update First Pass

--- src/spectrometer_gui/gui/spectrometer_controller.py
+++ src/spectrometer_gui/gui/spectrometer_controller.py
@@ -19,7 +19,7 @@ class ScanSignals(QObject):
     device_status_changed = Signal(str, DeviceStatus)  # device_id, DeviceStatus
     progress = Signal(float, str)
     scanning = Signal(bool, ScanType)
-    update_graph = Signal(ScanType, list)
+    update_graph = Signal(ScanType, list, list)  # scan_type, pos_array, max_list
 
 
 class SpectrometerController(QObject):
