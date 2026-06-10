487c03b1863e920f771db3dab6a31f12972fdccb
Trey Owen <treyeowen@gmail.com>
2026-06-10 17:28:47 -0500

    Connect new timing functions to GUI

--- src/spectrometer_gui/delay_generator_controller.py
+++ src/spectrometer_gui/delay_generator_controller.py
@@ -8,9 +8,11 @@ class DelayGeneratorController:
         self.initialized = False
 
     def initialize(self, config: Config):
-        self._visa_address = "GPIB0::9::INSTR"
+        self._devices: dict[str, dict] = {
+            "dg_1": {"address": "GPIB0::9::INSTR", "device": None},
+            "dg_2": {"address": "GPIB3::8::INSTR", "device": None}
+        }
         self._rm = visa.ResourceManager()
-        self._delay_generator = self._rm.open_resource(self._visa_address)
         self._frequency = 0
         self._delays = {"T0": "1", "A": "2", "B": "3", "C": "5", "D": "6"}
         self.stop_pulse()
@@ -29,8 +31,8 @@ class DelayGeneratorController:
         self.trigger_rate = config.delay_generator_controller.trigger_rate
 
     # Writes a command to the delay generator and returns the output
-    def _write_cmd(self, command):
-        return self._delay_generator.write(command)
+    def _write_cmd(self, name, command):
+        return self._devices[name]["device"].write(command)
 
     def set_delays(self, new_delays):
         self._delays = new_delays
@@ -45,32 +47,45 @@ class DelayGeneratorController:
 
     def start_trig(self):
         write_str = "TR 0, " + str(self._frequency)
-        self._write_cmd(write_str)
+        self._write_cmd("dg_1", write_str)
         write_str = "TM 0"
-        self._write_cmd(write_str)
+        self._write_cmd("dg_1", write_str)
         print("Activating internal trigger at ", str(self._frequency))
 
     def stop_trig(self):
         write_string = "TM 1; TL 1"
-        self._write_cmd(write_string)
+        self._write_cmd("dg_1", write_string)
 
     def start_pulse(self):
         write_string = "DT 3,2,100E-9"
-        self._write_cmd(write_string)
+        self._write_cmd("dg_1", write_string)
 
     def stop_pulse(self):
         write_string = "DT 3,2,0"
-        self._write_cmd(write_string)
+        self._write_cmd("dg_1", write_string)
 
     def set_trig(self):
         write_string = "TM 0; TR 0, " + str(self.trigger_rate)
-        self._write_cmd(write_string)
-    def SPDT_switch(self,width):    #in us # TODO: connect with second SRS
-        width = float(width) + 0.2
-        write_string = f"DT 5,6,{width}E-6" #goes to second SRS
-        self._write_cmd(write_string)
+        self._write_cmd("dg_1". write_string)
 
-    def gas_MW_delay(self,delay):   # TODO: connect with second SRS
+    def SPDT_switch(self, width):
+        width = float(width) + 0.2
+        write_string = f"DT 5,6,{width}E-6" # Goes to second SRS
+        self._write_cmd("dg_2", write_string)
+
+    def gas_MW_delay(self, delay):
         delay = float(delay)
-        write_string = f"DT 6,1{delay}E-3"    #in ms ; same command should go to both
-        self._write_cmd(write_string)
+        write_string = f"DT 6,1{delay}E-3"    # In ms ; same command should go to both
+        self._write_cmd("dg_1", write_string)
+        self._write_cmd("dg_2", write_string)
+
+    def _open_device(self, name: str, timeout: int = 5000) -> None:
+        current = devices[name]
+        if current["device"] is None:
+            res = self._rm.open_resource(current["address"])
+            res.timeout = timeout
+            current["device"] = res
+    
+    def _open_all_devices(self, timeout: int = 10000) -> None:
+        for device in devices:
+            self._open_device(device, timeout)
