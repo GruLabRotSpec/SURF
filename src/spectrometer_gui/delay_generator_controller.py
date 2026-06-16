eb2d76f5299e08f739bf931ec0768fc8a90a7118
cademiller24 <162280569+cademiller24@users.noreply.github.com>
2026-06-16 17:36:11 -0500

    Fix delay generator issues
    Fix class methods

--- src/spectrometer_gui/delay_generator_controller.py
+++ src/spectrometer_gui/delay_generator_controller.py
@@ -10,15 +10,18 @@ class DelayGeneratorController:
     def initialize(self, config: Config):
         self._devices: dict[str, dict] = {
             "dg_1": {"address": "GPIB0::9::INSTR", "device": None},
-            "dg_2": {"address": "GPIB3::8::INSTR", "device": None}
+            "dg_2": {"address": "GPIB3::8::INSTR", "device": None},
         }
         self._rm = visa.ResourceManager()
         self._frequency = 0
         self._delays = {"T0": "1", "A": "2", "B": "3", "C": "5", "D": "6"}
+
+        self._open_all_devices()
+
         self.stop_pulse()
 
         # Turn on external trigger
-        self._write_cmd("TM 1; TL 1")
+        self._write_cmd("dg_1", "TM 1; TL 1")
 
         self.update_config(config)
 
@@ -66,26 +69,26 @@ class DelayGeneratorController:
 
     def set_trig(self):
         write_string = "TM 0; TR 0, " + str(self.trigger_rate)
-        self._write_cmd("dg_1". write_string)
+        self._write_cmd("dg_1", write_string)
 
     def SPDT_switch(self, width):
         width = float(width) + 0.2
-        write_string = f"DT 5,6,{width}E-6" # Goes to second SRS
+        write_string = f"DT 5,6,{width}E-6"  # Goes to second SRS
         self._write_cmd("dg_2", write_string)
 
     def gas_MW_delay(self, delay):
         delay = float(delay)
-        write_string = f"DT 6,1{delay}E-3"    # In ms ; same command should go to both
+        write_string = f"DT 6,1{delay}E-3"  # In ms ; same command should go to both
         self._write_cmd("dg_1", write_string)
         self._write_cmd("dg_2", write_string)
 
     def _open_device(self, name: str, timeout: int = 5000) -> None:
-        current = devices[name]
+        current = self._devices[name]
         if current["device"] is None:
             res = self._rm.open_resource(current["address"])
             res.timeout = timeout
             current["device"] = res
-    
+
     def _open_all_devices(self, timeout: int = 10000) -> None:
-        for device in devices:
+        for device in self._devices:
             self._open_device(device, timeout)
